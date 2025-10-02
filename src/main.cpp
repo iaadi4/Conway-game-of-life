785ffa7598d5e4289bfc52b8876982e718fa9129
iaadi4 <adityasingh40675@gmail.com>
2025-10-03 02:16:47 +0530

    feat: add basic window display, fix: remove ball.cpp from makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 #include <raylib.h>
 
 int main() {
-    
+    Color GREY = {29, 29 , 29 ,255};
+    const int WINDOW_HEIGHT = 750;
+    const int WINDOW_WIDTH = 750;
+    int FPS = 12;
+
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Conway game of life");
+    SetTargetFPS(FPS);
+
+    // simulation window
+    while(WindowShouldClose() == false) {
+        // Drawing
+        BeginDrawing();
+        ClearBackground(GREY);
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
